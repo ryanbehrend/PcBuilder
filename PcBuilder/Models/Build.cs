88268b1fd42963fbd0c8b41ffcea8a75800200b9
Ryan Behrend <ryanjbehrend@gmail.com>
2025-09-30 10:26:14 -0500

    Add Build and Part models to PcBuilder.Models

---
+++ PcBuilder/Models/Build.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PcBuilder.Models
+{
+    public class Build
+    {
+        public int Id { get; set; }
+        public List<int> PartIds { get; set; } = new List<int>();
+    }
+}
---
+++ PcBuilder/Models/Part.cs
@@ -0,0 +1,11 @@
+﻿namespace PcBuilder.Models
+{
+    public class Part
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Specs { get; set; }
+        public decimal Price { get; set; }
+    }
+}
